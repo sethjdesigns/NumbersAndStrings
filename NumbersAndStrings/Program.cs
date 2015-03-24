d5364740b7dfffe05d78e0c8558f9f736e74a503
Seth Johnson <sethjdesigns@gmail.com>
2015-03-24 11:05:32 -0400

    Solution to Lab Question 2
    Solution to lab Question 2

--- NumbersAndStrings/Program.cs
+++ NumbersAndStrings/Program.cs
@@ -10,7 +10,21 @@ namespace NumbersAndStrings
     {
         static void Main(string[] args)
         {
+            var increment = 35;
+            var bankBalance = 0;
 
+            for (int i = 0; i < 365; i++) 
+            {
+                bankBalance += increment;
+                
+            }
+
+            Console.WriteLine("Your account balance after one year is {0:C2}.", bankBalance);
+            Console.WriteLine("Your account balance after two years is {0:C2}.", bankBalance * 2);
+            Console.WriteLine("Your account balance after five years is {0:C2}.", bankBalance * 5);
+            Console.WriteLine("Your account balance after ten years is {0:C2}.", bankBalance * 10);
+
+            Console.ReadLine();
 
         }
     }
