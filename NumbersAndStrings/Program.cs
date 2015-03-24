5152a34ace3ad52ea1221b9f72f768cf885e625b
Seth Johnson <sethjdesigns@gmail.com>
2015-03-24 09:50:01 -0400

    Maths
    Maths are hard sometimes.

---
+++ NumbersAndStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0xff;
+
+            int bigIndex = index * 255;
+        }
+    }
+}
