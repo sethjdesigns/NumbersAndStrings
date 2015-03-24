6772d90c97d318598cf84b2b072608b2eee68d0d
Seth Johnson <sethjdesigns@gmail.com>
2015-03-24 10:29:34 -0400

    Start of Lab
    Starting point of Lab

--- NumbersAndStrings/Program.cs
+++ NumbersAndStrings/Program.cs
@@ -10,25 +10,7 @@ namespace NumbersAndStrings
     {
         static void Main(string[] args)
         {
-            //int index = 10000;
 
-            //int oneThird = index / 3;
-
-            //int remainder = index % 3;
-
-            var increment = 1.0 / 3.0;
-            var total = 0.0;
-
-            for (int i = 0; i < 100000; i++) 
-            {
-                total = increment * (double)i;
-                Console.WriteLine(total);
-                if (i % 20 == 0)
-                {
-                    Console.WriteLine("Pausing...");
-                    //Console.ReadLine();
-                }
-            }
         }
     }
 }
